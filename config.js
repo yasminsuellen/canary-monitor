e96be92eeca24483e5bfb4234961b98c4af13e5b
Yasmin Suellen <eusuellen.yasmin@gmail.com>
2026-06-20 12:23:51 -0300

    chore: initial project structure

---
+++ config.js
@@ -0,0 +1,22 @@
+const services = [
+  {
+    name: "Auth API",
+    url: "https://jsonplaceholder.typicode.com/posts/1",
+    interval: 10000,
+    threshold: 500
+  },
+  {
+    name: "User Service",
+    url: "https://jsonplaceholder.typicode.com/users/1",
+    interval: 15000,
+    threshold: 800
+  },
+  {
+    name: "Posts API",
+    url: "https://jsonplaceholder.typicode.com/posts",
+    interval: 20000,
+    threshold: 1000
+  }
+]
+
+export default services
